00c7736dfe6d136db220a8ba2dd4152893d38be2
suciwrrnd <suciwirand@mhs.usk.ac.id>
2026-06-30 19:55:00 +0700

    Build to docs folder for Github Pages

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: './', // Mengubah ke relative path agar tidak blank di GitHub Pages
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
+  }
 })
